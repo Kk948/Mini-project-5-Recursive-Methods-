ead4b7c6ce118a00dfe262e1e3952e7d15f75adb
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2021-02-18 18:37:53 +0000

    I changed the variable of blood to sun

--- Main.java
+++ Main.java
@@ -1,13 +1,30 @@
+import java.util.Scanner;
 class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) 
+  {
+    System.out.println(" Welcome, we're here in the town to figure out whether you are a vampire of not!");
+    vampire();
   }
-  public static int blood(int vampires)
 
-   if (vampires = 4)
-    return 1;
-    //System.out.println("The blood supply is running low. Time to go hunting");
-  else
-    return 2 + blood(vampires - 3);
-   // System.out.println("The blood supply is good.");
+  public static int vampire(int sun){
 
+    System.out.println("How many minutes can you stand outside in the sun");
+    Scanner scan = new Scanner(System.in);
+    int sun = scan.nextInt();
+
+   if (sun  < 4)
+   {
+      System.out.println("I guess we were wrong.Sorry for burning your house down");
+      return true;
+      
+   }  
+      else
+   {
+     System.out.println("You're a vampire,get them!");
+      return Sun + vampire;
+      
+      
+   }
+  }
+  
 }
