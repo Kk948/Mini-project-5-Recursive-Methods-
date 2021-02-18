6ea86fd9f74c376dd21798225b84a55c9bd49359
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2021-02-18 18:08:35 +0000

    I finished the outline of my code

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 class Main {
   public static void main(String[] args) {
-    
   }
+  public static int blood(int vampires)
+
+   if (vampires = 4)
+    return 1;
+    //System.out.println("The blood supply is running low. Time to go hunting");
+  else
+    return 2 + blood(vampires - 3);
+   // System.out.println("The blood supply is good.");
+
 }
