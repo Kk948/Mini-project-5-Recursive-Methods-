0b65a2f97ddea3ec6888a44de6e3095c42185202
Katelynn Lauricello <katelynn.lauricello@patelhs.org>
2021-02-24 18:57:49 +0000

    I finished my project.

--- Main.java
+++ Main.java
@@ -1,30 +1,25 @@
 import java.util.Scanner;
+
 class Main {
-  public static void main(String[] args) 
-  {
+  public static void main(String[] args) {
     System.out.println(" Welcome, we're here in the town to figure out whether you are a vampire of not!");
-    vampire();
-  }
-
-  public static int vampire(int sun){
-
     System.out.println("How many minutes can you stand outside in the sun");
     Scanner scan = new Scanner(System.in);
     int sun = scan.nextInt();
-
-   if (sun  < 4)
-   {
-      System.out.println("I guess we were wrong.Sorry for burning your house down");
-      return true;
-      
-   }  
-      else
-   {
-     System.out.println("You're a vampire,get them!");
-      return Sun + vampire;
-      
-      
-   }
+    vampire(sun);
   }
-  
+  public static boolean vampire(int sun) {
+    Scanner scan = new Scanner(System.in);
+    if (sun <= 4) {
+      System.out.println("You're a vampire,get them!");
+      return true;
+
+    } else {
+        System.out.println("How many minutes can you stand outside in the sun");
+        sun = scan.nextInt();
+        return vampire( sun);
+
+    }
+  }
+
 }
